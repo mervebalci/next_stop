467cea96caa7296099e6361c93b94de712ccaa58
Merve Balci <mervebalci090@gmail.com>
2023-04-09 15:25:53 -0300

    Creating the homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,47 @@
+import Head from 'next/head'
+
 export default function Home() {
   return (
     <div>
-      <h1>Home page</h1>
-      <a href='/api/auth/signin'>login</a>
+      <Head>
+        <title>Best in Town</title>
+      </Head>
+
+      <div className='text-center'>
+        <h1 className='font-extrabold text-3xl mt-10'>The Best in Town</h1>
+        
+        <div className='grid md:grid-cols-3'>
+          <div>
+            <h2 className='mt-10 font-bold text-xl'>Restaurants</h2>
+
+            <ol className='mt-4 list-inside list-decimal'>
+              <li>Restaurant 1</li>
+              <li>Restaurant 2</li>
+              <li>Restaurant 3</li>
+            </ol>
+          </div>
+
+          <div>
+            <h2 className='mt-10 font-bold text-xl'>Hotels</h2>
+            
+            <ol className='mt-4 list-inside list-decimal'>
+              <li>Hotel 1</li>
+              <li>Hotel 2</li>
+              <li>Hotel 3</li>
+            </ol>
+          </div>
+          
+          <div>
+            <h2 className='mt-10 font-bold text-xl'>Things to do</h2>
+            
+            <ol className='mt-4 list-inside list-decimal'>
+              <li>Thing 1</li>
+              <li>Thing 2</li>
+              <li>Thing 3</li>
+            </ol>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
