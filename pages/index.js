b1cae1bc46530899eaf3834f402760b90e0daafd
Merve Balci <mervebalci090@gmail.com>
2023-05-11 21:24:52 -0300

    Displaying items from the database

---
+++ pages/index.js
@@ -0,0 +1,76 @@
+import Head from 'next/head'
+
+import prisma from 'lib/prisma'
+import { getItems } from 'lib/data'
+
+export default function Home({ restaurants, hotels, thingsToDo }) {
+  return (
+    <div>
+      <Head>
+        <title>Best in Town</title>
+        <meta name='description' content='Private Area' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <div className='text-center'>
+        <a href='/api/auth/signin'>login</a>
+        <h1 className='font-extrabold text-4xl mt-10'>The Best in Town</h1>
+        
+        <div className='flex flex-col md:grid md:grid-cols-3'>
+          
+          {restaurants && (
+            <div>
+              <h2 className='mt-10 font-bold text-2xl'>Restaurants</h2>
+
+              <ol className='mt-4 list-inside list-decimal'>
+                {restaurants.map((item, index) => (
+                  <li className='mt-1' key={index}>{item.name}</li>
+                ))}
+              </ol>
+            </div>
+          )}
+          
+
+          {hotels && (
+            <div>
+              <h2 className='mt-10 font-bold text-2xl'>Hotels</h2>
+          
+              <ol className='mt-4 list-inside list-decimal'>
+                {hotels.map((item, index) => (
+                  <li className='mt-1' key={index}>{item.name}</li>
+                ))}
+              </ol>
+            </div>
+          )}
+
+          
+          {thingsToDo && (
+            <div>
+              <h2 className='mt-10 font-bold text-2xl'>Things to do</h2>
+            
+              <ol className='mt-4 list-inside list-decimal'>
+                {thingsToDo.map((item, index) => (
+                  <li className='mt-1' key={index}>{item.name}</li>
+                ))}
+              </ol>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export async function getServerSideProps() {
+  const restaurants = await getItems(prisma, 'restaurant')
+  const hotels = await getItems(prisma, 'hotel')
+  const thingsToDo = await getItems(prisma, 'thing-to-do')
+
+  return {
+    props: {
+      restaurants,
+      hotels,
+      thingsToDo,
+    },
+  }
+}
